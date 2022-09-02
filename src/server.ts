d4bd98638f66b7e8d161304026236bb29d73bf2d
FabioJunik <fabiojunik@gmail.com>
2022-09-02 17:55:15 +0100

    Tratamento de exeção

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Response, Request } from "express";
+import "express-async-errors";
 
 import "./database"
 import { router } from "./routes";
@@ -9,5 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return response.status(400).json({
+            error:err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+})
 
 app.listen(5000, ()=>console.log("Server is running "))
